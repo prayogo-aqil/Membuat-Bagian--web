f1a104008356c776ff132f286ad43098aac66e72
Your Name <you@example.com>
2020-10-21 09:55:20 +0700

    Membuat-Bagian--web

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import RegisterBg from './image/register-aw.jpg';
+import LoginBg from './image/login-aw.jpg';
+
+export {RegisterBg, LoginBg};
---
+++ src/pages/Register/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './register.scss';
+import { RegisterBg } from '../../assets';
+import { Input, Button, Gap, Link } from '../../components';
+
+const Register = () => {
+  return (
+    <div className="main-page">
+      <div className="left">
+        <img src={RegisterBg} className="aw-image" alt="imageAw" />       
+      </div>
+      <div className="right">
+        <p className="title">Register</p>
+        <Input label="Full Name" placeholder="Full Name" />
+        <Gap height={18} />
+        <Input label="Email" placeholder="Email" />
+        <Gap height={18} />
+        <Input label="Password" placeholder="Password" />
+        <Gap height={50} />
+        <Button title="Register" />
+        <Gap height={100} />
+        <Link title="Kembali ke Login" />
+      </div>
+    </div>
+  )
+}
+
+export default Register
